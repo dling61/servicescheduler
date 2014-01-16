36a10d82b88c0f5561e7bc817f22cf4dc5f13066
dling61 <dling61@yahoo.com>
2014-01-15 21:57:56 -0800

    deploy

--- backend/Alert_service_Tble_1.2.0.sql
+++ backend/Alert_service_Tble_1.2.0.sql
@@ -1,1 +1,6 @@
+
+ALTER TABLE service ADD Start_Datetime INT(11) AFTER To_Date;
+
+ALTER TABLE service ADD End_Datetime INT(11) AFTER Start_Datetime;
+
 ALTER TABLE service ADD Last_Modified_Id INT(11) AFTER Last_Modified;
