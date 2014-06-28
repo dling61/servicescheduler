130ba8bbfb16942aa5abd96b46f2f79043bc4f07
dling61 <dling61@yahoo.com>
2014-06-28 16:32:50 -0700

    add SP to retrieve server setting

---
+++ sp/getServerSetting_Sp.sql
@@ -0,0 +1,24 @@
+DELIMITER $$
+  DROP PROCEDURE IF EXISTS getServerSetting;
+  CREATE PROCEDURE `getServerSetting`()
+  BEGIN
+  
+
+  -- Time Zone list
+  SELECT  t.Id id, t.Tz_Name tzname , t.Display_Name displayname, t.Display_Order displayorder, t.Abbr abbrtzname
+          FROM timezonedb t
+          WHERE t.Active_Flag = 1 order by 1;
+  
+
+  -- Alert list
+  SELECT  t.Id id, t.Alert_Name aname
+          FROM alertsetting t
+          WHERE t.Active_Flag = 1 order by 1;
+
+  -- Enforce update
+  SELECT a.id id, a.App_Version appversion, a.Enforce_Flag enforce, a.OS os, a.OS_version osversion
+	FROM appdevice a;
+
+ 
+  END $$
+  DELIMITER ;
