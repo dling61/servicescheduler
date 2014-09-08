4eb8c55f9d8a98c8beed23fa87155de2e2461230
dling61 <dling61@yahoo.com>
2014-09-08 14:51:17 -0700

    Adjust columns

--- backend/072014_Alter_service_Tble.sql
+++ backend/072014_Alter_service_Tble.sql
@@ -10,7 +10,9 @@ ALTER TABLE `service` DROP `Start_Time`;
 ALTER TABLE `service` DROP `Alert`;
 ALTER TABLE `service` DROP `SRepeat`;
 ALTER TABLE `service` DROP `From_Date`;
-ALTER TABLE `service` DROP `Sart_Datetime`;
+ALTER TABLE `service` DROP `To_Date`;
+ALTER TABLE `service` DROP `UTC_Off`;
+ALTER TABLE `service` DROP `Start_Datetime`;
 ALTER TABLE `service` DROP `End_Datetime`;
 
 
