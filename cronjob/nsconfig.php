263baa4359554799e5cc8507a9fb6b6bcf1f7634
dling61 <dling61@yahoo.com>
2014-09-18 14:41:18 -0700

    update GCM API key

--- cronjob/nsconfig.php
+++ cronjob/nsconfig.php
@@ -1,5 +1,5 @@
 <?php
     define('ANDROID_NS_SERVER_URL', 'https://android.googleapis.com/gcm/send');
-    define('GOOGLE_API_KEY', 'AIzaSyD6JfNCyz83LFHzrBVbzhHtb7uv08CWHIw');
-  
+    define('GOOGLE_API_KEY', 'AIzaSyA-GnIymHjsiREf4I90XVRMUICYwDHvaRA');
+
 ?>
