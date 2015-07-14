14bf6d04bb39fb064ba5723cd1c54d9a1f7ed430
dling61 <dling61@yahoo.com>
2015-07-14 15:03:01 -0700

    add upload profile api. Resize of image should be done on client

--- docroot/constants.php
+++ docroot/constants.php
@@ -12,6 +12,36 @@
 	define('FEEDBACK_EMAIL', 'ding.dongling@gmail.com');
 	// define a profile image directory
 	define ('PROFILE_SERVER', 'http://127.0.0.1/filesvc/');
+	define ('FILE_LOCATION', 'c:\\localweb\\');
+	
+	
+	/*** Test Environment 
+    define('DB_NAME', 'testscheduler1');
+    define('DB_USER', 'testscheduler1');
+    define('DB_PASS', 'Scheduler2012#');
+    define('DB_SERVER', 'testscheduler1.db.9936855.hostedresource.com');
+	define('DEBUG_FLAG', '1');
+	
+	define('PUSH_MODE', 'development');
+	define('LOG_LOCATION', 'log');
+	define('REST_SERVER', 'appitest1.servicescheduler.net');
+
+	define('FEEDBACK_EMAIL', 'ding.dongling@gmail.com');
+	// define a profile image directory
+	define ('PROFILE_SERVER', 'http://test.cschedule.com/profile/');
+	define ('FILE_LOCATION', '../cscheduletest/profile/');
+	****/
+	
+	//Define our id-key pairs
+	$applications = array(
+	    // for IOS
+		'IOS' => '28e336ac6c9423d946ba02dddd6a2632', //randomly generated app key 
+		// for Andriod
+		'ANDROID' => '28e336ac6c9423d946ba02d19c6a2633', //randomly generated app key 
+		// for Web App
+		'WEB' => '28e336ac6c9423d946ba02d19c6a2634', //randomly generated app key 
+	);
+?>
 	
 	//Define our id-key pairs
 	$applications = array(
