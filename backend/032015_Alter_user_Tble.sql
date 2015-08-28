f0c55a1860f8ae85ce45dc498a4acd5592370d35
dling61 <dling61@yahoo.com>
2015-08-28 13:54:44 -0700

    Add last_modified_id

--- backend/032015_Alter_user_Tble.sql
+++ backend/032015_Alter_user_Tble.sql
@@ -7,3 +7,5 @@ ALTER TABLE user ADD Profile varchar(500) AFTER Mobile;
 
 ALTER TABLE user ADD Active tinyint(1) default 1 AFTER Verified;
 
+ALTER TABLE user ADD Last_Modified_Id int(11) default 0 AFTER Last_Modified;
+
