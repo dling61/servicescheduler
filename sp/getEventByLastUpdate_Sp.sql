0699411fca926fb9af7c00aad7223a06e6097fb0
dling61 <dling61@yahoo.com>
2015-10-05 21:42:19 -0700

    Get event returning base events

--- sp/getEventByLastUpdate_Sp.sql
+++ sp/getEventByLastUpdate_Sp.sql
@@ -18,7 +18,7 @@
   -- Event list
   SELECT Schedule_Id eventid, Service_Id communityid, Schedule_Name eventname, Creator_Id creatorid, Description description, FROM_UNIXTIME(Start_Datetime) startdatetime,
           FROM_UNIXTIME(End_Datetime) enddatetime, Alert alert, Tz_Id tzid, Is_Deleted isdeleted, REvent_Id reventid, Location location, Host host, Created_Time createdtime, Last_Modified lastmodified
-      FROM schedule where Service_Id = communityid and Last_Modified > p_lastupdate order by schedule.Start_Datetime;
+      FROM schedule where Service_Id = communityid and Last_Modified > p_lastupdate and Is_Deleted = 0 order by schedule.Start_Datetime;
   
   -- create a temp table
   -- task list
@@ -29,10 +29,16 @@
 
   SELECT distinct taskid, eventid, taskname, assignallowed, assignedgroup, description from tmp_task order by eventid;
 
-  -- assignment list
-  SELECT ta.Task_Id taskid, ta.Schedule_Id eventid, ta.User_Id userid, u.User_Name username, u.Profile userprofile, ta.Confirm confirm 
-     FROM taskassigned ta, user u
+  -- taskhelper list
+  SELECT ta.TaskHelper_Id taskhelperid, ta.Task_Id taskid, ta.Schedule_Id eventid, ta.User_Id userid, u.User_Name username, u.Profile userprofile, ta.Status status 
+     FROM taskhelper ta, user u
       WHERE  ta.Schedule_Id in (select distinct eventid from tmp_task) and ta.Is_Deleted = 0 and ta.User_Id = u.User_Id order by ta.Schedule_Id;
+	  
+  SELECT distinct bs.REvent_Id beventid, bs.REvent_Name eventname, bs.REvent_StartTime starttime, bs.REvent_EndTime endtime, bs.REvent_Location location,
+		bs.REvent_Host host, bs.REvent_Tz_Id tzid, bs.Frequency frequency, bs.RepeatInterval binterval, bs.From bfrom, bs.To bto,
+		bs.Is_Deleted isdeleted, bs.Created_Time createdtime, bs.Last_Modified lastmodified
+	FROM baseschedule bs LEFT JOIN schedule sc
+	ON bs.REvent_Id = sc.REvent_Id and sc.Service_Id = communityid and bs.Last_Modified > p_lastupdate and bs.Is_Deleted = 0 order by bs.REvent_Id;
 
   drop table tmp_task;
 
