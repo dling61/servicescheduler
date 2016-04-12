092aaef0525866433424f54970629f5f24d667cc
dling61 <dling61@yahoo.com>
2016-04-12 15:45:00 -0700

    remove the configuration information from the constants.php

--- docroot/constants.php
+++ docroot/constants.php
@@ -1,22 +1,5 @@
 <?php
-
-    /**
-	 *    Database Connection
-	 *    TBD: Change them for the environment
-	 */
-    define('DB_NAME', 'cschedule2016');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_SERVER', '127.0.0.1');
-    define('DEBUG_FLAG', '0');
-	
-	
-	/**
-	 *   Profile Image Location
-	 *   TBD: Change them for the environment
-	 */
-	define ('PROFILE_SERVER', 'http://test.cschedule.com/profile/');
-	define ('FILE_LOCATION', '../cscheduletest/profile/');
+    require_once('config.php');
 	
 	/**
 	 *    Session management
@@ -32,8 +15,7 @@
 	define('NORMAL_SESSION', '1800');
 	
 	define('LOGIN_SESSION_EXPIRE', '-1');
-	// TBD: Change them for the environment
-	define('CLIENT_SERVER', 'http://www.cschedule.org');
+
 	
 	/**
 	 *  IOS push notification 
@@ -48,15 +30,4 @@
 	define('FEEDBACK_EMAIL', 'ding.dongling@gmail.com');
 	define('LOG_LOCATION', 'log');
 	
-	/**
-	 *    Security Keys
-	 */
-	$applications = array(
-	    // for IOS
-		'IOS' => '28e336ac6c9423d946ba02dddd6a2632', //randomly generated app key 
-		// for Andriod
-		'ANDROID' => '28e336ac6c9423d946ba02d19c6a2633', //randomly generated app key 
-		// for Web App
-		'WEB' => '28e336ac6c9423d946ba02d19c6a2634', //randomly generated app key 
-	);
 ?>
